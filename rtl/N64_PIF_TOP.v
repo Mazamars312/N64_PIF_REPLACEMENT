bedb4f0cddd10bf6abc0a3a171f2fe306d55b7fb
Murray Aickin <murray.aickin@boomweb.co.nz>
2019-11-07 08:50:11 +1000

    More updates
    Added more to the README file on how things work and started on the PIF top to add all of the cores to it.
    
    This includes the addressing.
    
    Need to add the ROM core and some ram locations. Aswell as a data ready line for each core.

--- rtl/N64_PIF_TOP.v
+++ rtl/N64_PIF_TOP.v
@@ -24,9 +24,63 @@ module N64_PIF_TOP(
     );
     
     wire [15:0] address;
-    wire [7:0]  cpu_data_in, cpu_data_out;
+    reg  [7:0]  cpu_data_in;
+    wire [7:0]  cpu_data_out, ram_data_out, n64_controller_data_out, pif_ram_data_out, eeprom_data_out, rom_data_out, crc_rom_data_out, n64_interface_reg_data_out;
     wire        cpu_write;
     wire        ready;
+    
+    reg         controller_oe, eeprom_oe, ram_oe, pif_ram_oe, crc_rom_oe, n64_interface_reg_oe, rom_oe, fake_oe;
+    
+    
+    
+    always @* begin
+        controller_oe <= 'b0;
+        eeprom_oe <= 'b0;
+        ram_oe <= 'b0;
+        pif_ram_oe <= 'b0;
+        crc_rom_oe <= 'b0;
+        n64_interface_reg_oe <= 'b0;
+        rom_oe <= 'b0;
+        if (address <= 15'h00ff) begin
+            casez (address[7:0])
+//                8'b0zzz_zzzz    : pif_ram_oe <= 1'b1; we do this with the default
+                8'h8z,
+                8'h9z           : crc_rom_oe <= 1'b1;
+                8'hAz           : controller_oe <= 1'b1;
+                8'hBz           : eeprom_oe <=1'b1;
+                8'hCz           : n64_interface_reg_oe <= 1'b1;
+                8'hDz           : fake_oe <= 1'b1;
+                8'hEz           : fake_oe <= 1'b1;
+                8'hFz           : fake_oe <= 1'b1;
+                default     : pif_ram_oe <= 1'b1;
+            endcase
+        end
+        else begin
+            if (address >= 15'h0400) crc_rom_oe <= 1'b1;
+            else ram_oe <= 1'b1;
+        end
+    end
+    
+    always @* begin
+        cpu_data_in <= 15'h0000;
+        if (address <= 15'h00ff) begin
+            casez (address[7:0])
+                8'h8z,
+                8'h9z           : cpu_data_in <= crc_rom_data_out;
+                8'hAz           : cpu_data_in <= n64_controller_data_out;
+                8'hBz           : cpu_data_in <= eeprom_data_out;
+                8'hCz           : cpu_data_in <= n64_interface_reg_data_out;
+                8'hDz           : cpu_data_in <= 15'h0000;
+                8'hEz           : cpu_data_in <= 15'h0000;
+                8'hFz           : cpu_data_in <= 15'h0000;
+                default         : cpu_data_in <= pif_ram_data_out;
+            endcase
+        end
+        else begin
+            if (address >= 15'h0400) cpu_data_in <= ram_data_out;
+            else cpu_data_in <= rom_data_out;
+        end
+    end
     
     
     cpu6502 cpu6502( 
@@ -40,5 +94,21 @@ module N64_PIF_TOP(
     .NMI    (1'b0), 
     .RDY    (ready) );
     
+    
+    N64_controller_top N64_controller_top(
+    .clk          (clk),
+    .reset_l      (reset_l),
+    
+    .joy1         (joy1),
+    .joy2         (joy2),
+    .joy3         (joy3),
+    .joy4         (joy4),
+    
+    .address      (address),
+    .data_in_bus  (cpu_data_out),
+    .write        (write),
+    .ce           (ce),
+    .data_out_bus (n64_controller_data_out)
+    );
     
 endmodule
